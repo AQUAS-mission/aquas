f406752e04d8904d536570c36359a021087b2c98
kd993595 <kepedu17@gmail.com>
2025-03-12 20:04:48 -0400

    update python file for raspberry pi

--- kevin_data_backend/main.py
+++ kevin_data_backend/main.py
@@ -1,39 +1,22 @@
 import requests
-import csv
-import time
 
-API_ENDPOINT = "http://127.0.0.1:3030/upload"
+url = 'http://<STRAPILocalIP>:<PORT>/api/aqua-sreading-tests'
 
-API_KEY = "XXXXXXXXXXXXXXXXX"
+myobj = {"data":{
+  "Temperature": 25.5,
+  "device_id": 1512313531,
+  "timestamp": "2025-02-26T12:34:56.789Z"
+}
+}
 
-with open("MOCK_DATA2.csv","r") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        # print(f"{row[0]} --- {row[1]} --- {row[2]} --- {row[3]} raw:{','.join(row)}\n")
-        rawdata = ','.join(row)
-        line_count += 1
-        try: 
-            r = requests.post(url=API_ENDPOINT, data=rawdata, headers={"Content-Type": "text/csv",'Authorization': API_KEY},timeout=1)
-            print(r.status_code)
-            print(r.content)
-        except requests.exceptions.HTTPError as errh:
-            print ("Http Error:",errh)
-        except requests.exceptions.ConnectionError as errc:
-            print ("Error Connecting:",errc)
-        except requests.exceptions.Timeout as errt:
-            print ("Timeout Error:",errt)
-        except requests.exceptions.RequestException as err:
-            print ("OOps: Something Else",err)
-        time.sleep(10)
-        if line_count >= 100:
-            break
-    print(f'Processed {line_count} lines.')
+x = requests.post(url,headers={"Content-Type": "application/json"},json=myobj)
+print(x.content)
 
 
 
 """
-#combine eventually
+# Code on the raspberry pi to read the arduino
+# combine eventually
 import serial
 import datetime
 import pytz
@@ -47,7 +30,7 @@ def nowtime():
     return epoch_time_secs
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) #ttyACM0 is the lsusb for arduino connected
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting > 0:
